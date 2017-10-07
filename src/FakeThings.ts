608598b31c99dc3e78ba3126853f5cefd2d4a281
Hans Otto Wirtz <hansottowirtz@gmail.com>
2017-10-07 11:30:26 +0200

    Only support browsers with Map#values

--- src/FakeThings.ts
+++ src/FakeThings.ts
@@ -4,35 +4,38 @@ declare const Map: any;
 
 export type FakeMapKey = string|number;
 
+const supportsMap = typeof Map !== 'undefined' && (new Map()).values;
+
 export class FakeMap {
-  hasRealMap: boolean;
-  map: any;
+  readonly hasRealMap: boolean;
+  readonly map: any;
 
   constructor() {
-    this.hasRealMap = typeof Map !== 'undefined';
-    this.map = this.hasRealMap ? new Map() : {};
+    // in IE 11, Map#values doesn't exist. Don't bother about that.
+    this.hasRealMap = supportsMap;
+    this.map = supportsMap ? new Map() : {};
   }
 
   get(k: FakeMapKey): any {
-    return this.hasRealMap ? this.map.get(k) : this.map[k];
+    return supportsMap ? this.map.get(k) : this.map[k];
   }
 
   set(k: FakeMapKey, v: any): this {
-    if (this.hasRealMap) { this.map.set(k, v) } else { this.map[k] = v };
+    if (supportsMap) { this.map.set(k, v) } else { this.map[k] = v };
     return this;
   }
 
   has(k: FakeMapKey): boolean {
-    return this.hasRealMap ? this.map.has(k) : !!this.map[k];
+    return supportsMap ? this.map.has(k) : !!this.map[k];
   }
 
   values(): any[] {
     // if Map is there, Array.from should also be there
-    return this.hasRealMap ? (Array as any as FakeArrayConstructor).from(this.map.values()) : Util.values(this.map);
+    return supportsMap ? (Array as any as FakeArrayConstructor).from(this.map.values()) : Util.values(this.map);
   }
 
   delete(k: FakeMapKey): void {
-    if (this.hasRealMap) { this.map.delete(k) } else { delete this.map[k]; }
+    if (supportsMap) { this.map.delete(k) } else { delete this.map[k]; }
   }
 }
 
