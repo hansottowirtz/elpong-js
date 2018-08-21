a7a41f94bd9126fe511bbc0254f4b03cc60721d4
Hans Otto Wirtz <hansottowirtz@gmail.com>
2018-08-21 15:01:25 +0800

    Update file locations

--- karma.conf.js
+++ karma.conf.js
@@ -116,7 +116,7 @@ module.exports = function(config) {
       config.files.unshift('node_modules/angular/angular.js', 'node_modules/angular-mocks/angular-mocks.js');
       break;
     default: // fetch
-      config.files.unshift('node_modules/fetch-mock/es5/client-browserified.js', 'node_modules/promise-polyfill/promise.js', 'node_modules/whatwg-fetch/fetch.js');
+      config.files.unshift('node_modules/promise-polyfill/dist/polyfill.js', 'node_modules/whatwg-fetch/fetch.js', 'node_modules/fetch-mock/dist/es5/client-bundle.js');
   }
   if (process.env.TRAVIS) {
     config.reporters.push('saucelabs')
