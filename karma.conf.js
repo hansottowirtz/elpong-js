5004c292d344dd171add054a2af0297605868c57
Hans Otto Wirtz <hansottowirtz@gmail.com>
2018-08-23 18:11:38 +0800

    Karma hangs

--- karma.conf.js
+++ karma.conf.js
@@ -77,7 +77,7 @@ module.exports = function(config) {
     reporters: ['progress'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
     autoWatch: false,
     browsers: Object.keys(customLaunchers), // overridden by certain gulp tasks
     singleRun: true,
