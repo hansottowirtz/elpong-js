0a190f6f086be88ff23354be6eabcbc29fbfe62b
Hans Otto Wirtz <hansottowirtz@gmail.com>
2018-08-23 21:15:53 +0800

    Fix example apps

--- examples/node-app/index.js
+++ examples/node-app/index.js
@@ -1,3 +1,11 @@
 const elpong = require('elpong').default;
 
-console.log(elpong);
+const obj = {"name": "test-app", "selector": "id", "collections": {"pigs": {"fields": {"id": {}, "name": {}}}}};
+
+const scheme = elpong.add(obj);
+scheme.select('pigs').build({id: 3, name: 'Bob'});
+
+const pig = elpong.get('test-app').select('pigs').find(3);
+
+console.log(pig.fields.name);
+
